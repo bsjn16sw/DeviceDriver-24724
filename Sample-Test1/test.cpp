121844ee0efaa70ccb6900c9c0dd4a883dd13693
sbin.jeon <sbin.jeon@samsung.com>
2024-07-05 11:22:40 +0900

    [refactor] Introduce TestFixture

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -9,58 +9,47 @@ public:
 	MOCK_METHOD(void, write, (long, unsigned char), (override));
 };
 
-TEST(TestCaseName, Flash에서Read가5번호출되어야PASS) {
+class TestFixture : public testing::Test {
+public:
 	MockFlashMemoryDevice mk;
+	DeviceDriver dd{ &mk };
+};
 
+TEST_F(TestFixture, Flash에서Read가5번호출되어야PASS) {
 	EXPECT_CALL(mk, read, (0xA), ())
 		.Times(5);
 
-	DeviceDriver dd{ &mk };
 	dd.read(0xA);
 }
 
-TEST(TestCaseName, Flash에서Read가5번중다르게리턴하는게있으면EXCEPTION) {
-	MockFlashMemoryDevice mk;
-
+TEST_F(TestFixture, Flash에서Read가5번중다르게리턴하는게있으면EXCEPTION) {
 	EXPECT_CALL(mk, read, (0xA), ())
 		.WillOnce(testing::Return(1))
 		.WillRepeatedly(testing::Return(2));
-
-	DeviceDriver dd{ &mk };
 
 	EXPECT_THROW({
 		dd.read(0xA);
 		}, ReadFailException);
 }
 
-TEST(TestCaseName, Write전에비어있는지Read해야함) {
-	MockFlashMemoryDevice mk;
-
+TEST_F(TestFixture, Write전에비어있는지Read해야함) {
 	EXPECT_CALL(mk, read, (0xA), ())
 		.Times(1)
 		.WillRepeatedly(testing::Return(0xFF)); // Avoid exception in dd.write()
 
-	DeviceDriver dd{ &mk };
 	dd.write(0xA, 'A');
 }
 
-TEST(TestCaseName, Write할때비어있어서PASS) {
-	MockFlashMemoryDevice mk;
-
+TEST_F(TestFixture, Write할때비어있어서PASS) {
 	EXPECT_CALL(mk, read)
 		.WillRepeatedly(testing::Return(0xFF));
 
-	DeviceDriver dd{ &mk };
 	dd.write(0xA, 'A');
 }
 
-TEST(TestCaseName, Write할때비어있지않아서EXCEPTION) {
-	MockFlashMemoryDevice mk;
-
+TEST_F(TestFixture, Write할때비어있지않아서EXCEPTION) {
 	EXPECT_CALL(mk, read, (0xA), ())
 		.WillRepeatedly(testing::Return('B'));
-
-	DeviceDriver dd{ &mk };
 
 	EXPECT_THROW({
 		dd.write(0xA, 'A');
