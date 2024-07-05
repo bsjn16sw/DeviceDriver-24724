7644d6d5c2b7c35204a57fb928d0df50fccee559
sbin.jeon <sbin.jeon@samsung.com>
2024-07-05 10:22:00 +0900

    [feature] DeviceDriver.read() needs FlashMemoryDevice.read() calls 5 times

--- Project41/DeviceDriver.cpp
+++ Project41/DeviceDriver.cpp
@@ -1,4 +1,6 @@
 #include "DeviceDriver.h"
+#include <unordered_set>
+
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
@@ -6,7 +8,12 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    m_hardware->read(address);
+    m_hardware->read(address);
+    m_hardware->read(address);
+    m_hardware->read(address);
+    m_hardware->read(address);
+    return 0;
 }
 
 void DeviceDriver::write(long address, int data)
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,8 +1,20 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../Project41/FlashMemoryDevice.h"
+#include "../Project41/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-  EXPECT_THAT(1, 1);
+class MockFlashMemoryDevice : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long), (override));
+	MOCK_METHOD(void, write, (long, unsigned char), (override));
+};
+
+TEST(TestCaseName, Flash에서Read가5번호출되어야PASS) {
+	MockFlashMemoryDevice mk;
+
+	EXPECT_CALL(mk, read, (0xA), ())
+		.Times(5);
+
+	DeviceDriver dd{ &mk };
+	dd.read(0xA);
 }
